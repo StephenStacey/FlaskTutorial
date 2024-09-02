988abfef6c6b8aba4e9c8b4faf38f0b7972f34d3
StephenStacey <stacey_stephen@hotmail.com>
2024-09-02 15:46:20 +0100

    updated git ignore file to include stock items

--- Flask sample.py
+++ Flask sample.py
@@ -1,1 +1,13 @@
+# http://127.0.0.1:5000
+
 from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route for the home function
+@app.route("/")
+def home():
+    return "Hello! This is the main page <h1>HELLO</h1>"
+
+if __name__ == "__main__":
+    app.run()
